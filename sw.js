d748fb7f5e09dd0a2631042df4a4281236cf6d6e
副总工作台 <workbench@example.com>
2026-04-07 16:37:35 +0800

    Initial commit: 副总工作台 PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Service Worker for 副总工作台 PWA
+const CACHE_NAME = 'workbench-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// 安装
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// 激活
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
